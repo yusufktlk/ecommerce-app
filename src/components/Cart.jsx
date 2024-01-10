d7672c8dfaf826a13abf5f1c3c26c00d382648aa
Yusuf <yusufbjk__44@hotmail.com>
2024-01-10 15:54:06 +0300

    changed text

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,16 +11,16 @@ function Cart() {
   const dispatch = useDispatch(clearCart)
 
   return (
-    <div className='bg-black w-[350px] overflow-hidden overflow-y-auto h-[500px] fixed  z-40 mt-14  right-0 rounded-l-xl  '>
+    <div className='bg-black w-[400px] overflow-hidden overflow-y-auto h-[500px] fixed  z-40 mt-14 right-0 rounded-l-xl  '>
         <hr className='border-black border-2' />
         <h1 className='text-center text-lg font-bold mt-4 underline mb-2 '>Cart</h1>
         <div className='flex flex-col gap-y-4 items-center'>
             {
               basket.map((basket,key) => (
-                <div key={key} className='flex gap-x-4 items-center text-[12px]'>
+                <div key={key} className='flex justify-between gap-x-4 mx-2 items-center text-[12px] text-left w-[350px] bg-slate-800 p-2 rounded-xl'>
                   <h1 className='w-32'>{basket.title}</h1>
                   <h1 className='text-red-600 text-sm'>{basket.price} $</h1>
-                  <h1>Adet:{basket.quantity}</h1>
+                  <h1>Unit: {basket.quantity}</h1>
                   <img src={basket.image} className='w-16 h-16 rounded-lg' />
                 </div>
               ))
@@ -30,7 +30,7 @@ function Cart() {
         <div className='text-sm text-center font-bold mb-4 mt-4'>
             <h5>Total Units: {totalQuantity}</h5>
             <h5>Total Price: <span className='text-green-500'>{totalPrice.toFixed(2)}$</span> </h5>
-            <div className='flex justify-between mx-10'>
+            <div className='flex justify-between mx-8'>
               <button className='bg-[#1C1C1D] text-white rounded-xl w-32 h-8 items-center mt-2 hover:scale-110 duration-500 '>Buy</button>
               <button onClick={() => dispatch(clearCart())} className='bg-[#1C1C1D] text-white rounded-xl w-32 h-8 items-center mt-2 hover:scale-110 duration-500 '>Delete Items</button>
             </div>
